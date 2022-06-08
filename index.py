b4aba6f0527d5c6f49f223e4d56509447c1d822b
Eoin Harkin <eharky@gmail.com>
2022-06-08 17:56:54 +0100

    writes users to .txt file

--- index.py
+++ index.py
@@ -8,6 +8,26 @@ names = json.loads(open('names.json').read())
 for name in names:
     extra = ''.join(random.choice(string.digits) for _ in range(6))
     username = name.lower()
-    email = username + extra + '@gmail.com'
+    emailLink = ['@gmail.com', '@yahoo.com', '@hotmail.com','@hotmail.co.uk', '@outlook.co.uk', '@outlook.com']
+    emailLink = random.choice(emailLink)
+    email = username + extra + emailLink
+    password = extra
 
-    print("Username:",username," Email:" , email)
+    def object():
+     d = dict()
+     d['username'] = username
+     d['email'] = email
+     d['password'] = password
+     return d
+    print(json.dumps(object()))
+
+    f = open("demofile.txt", "a")
+    f.write(json.dumps(object()) + "\n")
+    f.close()
+
+    f = open("demofile.txt", "r")
+    print(f.read())
+
+    
+
+
